8ebd02d00cec3fd489186e3f0337fafba9f19015
Jari Jokinen <jari.jokinen@j2.fi>
2023-07-23 16:02:29 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# rn-starter
+
+React Native starter.
+
+## License
+
+MIT License. Copyright (c) 2023 [Jari Jokinen](https://jarijokinen.com).  See
+[LICENSE](https://github.com/jarijokinen/rn-starter/blob/main/LICENSE.txt)
+for further details.
---
+++ src/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { SafeAreaView, View, Text } from 'react-native';
+
+const App = (): JSX.Element => {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>Hello, World!</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default App;
