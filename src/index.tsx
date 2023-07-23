6dbdb4245cb769e03985f16289da12106fe8368f
Jari Jokinen <jari.jokinen@j2.fi>
2023-07-23 18:08:27 +0300

    Add navigation

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,40 @@
 import React from 'react';
-import { SafeAreaView, View, Text } from 'react-native';
+
+import { PaperProvider, IconButton } from 'react-native-paper';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import HomeScreen from '@/screens/HomeScreen';
+import SettingsScreen from '@/screens/SettingsScreen';
+
+const Stack = createNativeStackNavigator();
 
 const App = (): JSX.Element => {
   return (
-    <SafeAreaView>
-      <View>
-        <Text>Hello, World!</Text>
-      </View>
-    </SafeAreaView>
+    <PaperProvider>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen
+            name="Home"
+            component={HomeScreen}
+            options={({ navigation }) => ({
+              headerTitle: 'Home',
+              headerRight: () => (
+                <IconButton
+                  icon="cog"
+                  onPress={() => navigation.navigate('Settings')}
+                />
+              )
+            })}
+          />
+          <Stack.Screen
+            name="Settings"
+            component={SettingsScreen}
+            options={{ headerTitle: 'Settings' }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </PaperProvider>
   );
 };
 
---
+++ src/screens/SettingsScreen.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const SettingsScreen = (): JSX.Element => {
+  return (
+    <View>
+      <Text>Settings</Text>
+    </View>
+  );
+};
+
+export default SettingsScreen;
