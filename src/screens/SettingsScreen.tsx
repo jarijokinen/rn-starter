206509d250e38ddfad28c935fbd94d92d101ecc0
Jari Jokinen <jari.jokinen@j2.fi>
2023-07-23 20:07:26 +0300

    Add settings feature

--- src/screens/SettingsScreen.tsx
+++ src/screens/SettingsScreen.tsx
@@ -1,10 +1,35 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { View } from 'react-native';
+import { List, Switch } from 'react-native-paper';
+import { mmkv } from '@/utils/mmkv';
+
+const settingSwitch = (value: boolean, handler: () => void): JSX.Element => (
+  <Switch value={value} onValueChange={handler} />
+);
 
 const SettingsScreen = (): JSX.Element => {
+  const [settingOne, setSettingOne] = useState(false);
+
+  useEffect(() => {
+    loadSettingOne();
+  }, []);
+
+  const loadSettingOne = () => {
+    setSettingOne(mmkv.getBoolean('setting.one') === true);
+  };
+
+  const onToggleSettingOne = () => {
+    const val = !settingOne;
+    setSettingOne(val);
+    mmkv.set('setting.one', val);
+  };
+
   return (
     <View>
-      <Text>Settings</Text>
+      <List.Item
+        title="Setting One"
+        right={() => settingSwitch(settingOne, onToggleSettingOne)}
+      />
     </View>
   );
 };
